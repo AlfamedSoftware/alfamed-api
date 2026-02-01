f76ff8f5fd0d63db18f1742855173f6c1bd052f9
Vinicius Engelmann <117754448+viniieng@users.noreply.github.com>
2026-02-01 13:31:52 -0300

    feat: adjust drizzly config

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { env } from '@/env'
 
 export default defineConfig({
-    schema: "./src/db/schemas/**",
+    schema: "./src/db/schema/**",
     out: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
--- api/src/auth.ts
+++ api/src/auth.ts
@@ -2,11 +2,14 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db/client";
 
-export const auth = betterAuth(
-    {
-        database: drizzleAdapter(db, {
-            provider: "pg",
-            usePlural: true,
-        }),
-    }
-)
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg",
+        usePlural: true,
+    }),
+    advanced: {
+        database: {
+            generateId: false,
+        },
+    },
+});
