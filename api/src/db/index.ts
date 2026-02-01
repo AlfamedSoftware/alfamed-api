1c5dd16afebc99107050a7e47cdd49ec42263a51
Vinicius Engelmann <vinicius.engelmann@gmail.com>
2026-02-01 01:51:26 -0300

    feat: add conection supabase and migration by better auth schemas

--- api/src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
-
-const supabaseUrl = process.env.SUPABASE_URL!;
-console.log(supabaseUrl);
-const client = postgres(process.env.SUPABASE_URL!);
-export const db = drizzle(client);
