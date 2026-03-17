c38dfdad7fffe18d11c61560c6f7d9a6e4cb3585
ThiagoRdrgs00 <thiagorodrigues221100@gmail.com>
2026-03-17 20:17:13 -0300

    B017 - Create migration procedures

---
+++ src/db/migrations/0012_procedures.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "procedures" (
+    "id" text PRIMARY KEY NOT NULL,
+    "name" text NOT NULL,
+    "description" text,
+    "code" text NOT NULL,
+    "price" numeric(12,2) NOT NULL,
+    "is_active" boolean DEFAULT true NOT NULL,
+    "created_at" timestamp DEFAULT now() NOT NULL,
+    "updated_at" timestamp DEFAULT now() NOT NULL
+);
