ccbc59643a5ab78e10de6034adf1448dd72473be
Vinicius Engelmann <viniengelmann@gmail.com>
2026-05-31 20:11:14 -0300

    att migrations nick name

--- src/db/migrations/0011_clumsy_moondragon.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "cpf" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "social_name" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "birthdate" timestamp;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "birthdate" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN IF NOT EXISTS "phone" text;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "phone" SET NOT NULL;
--- src/db/migrations/0020_roles.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "roles" (
-    "id" text PRIMARY KEY NOT NULL,
-    "description" text NOT NULL,
-    "key" text NOT NULL,
-    "is_active" boolean DEFAULT true NOT NULL,
-    "internal" boolean DEFAULT false NOT NULL,
-    "created_at" timestamp DEFAULT now() NOT NULL,
-    "updated_at" timestamp DEFAULT now() NOT NULL
-);
