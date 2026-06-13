3627ba210c171eb19e6d6e8371aac4d2444fcb25
ThiagoRdrgs00 <thiagorodrigues221100@gmail.com>
2026-06-13 09:30:59 -0300

    Prod - Correção para rodar migrations

--- src/db/migrations/0029_specialties_unit_name_unique.sql
+++ src/db/migrations/0029_specialties_unit_name_unique.sql
@@ -1,3 +1,1 @@
-ALTER TABLE "specialties" DROP CONSTRAINT IF EXISTS "specialties_name_unique";--> statement-breakpoint
-DROP INDEX IF EXISTS "specialties_name_unique";--> statement-breakpoint
-CREATE UNIQUE INDEX IF NOT EXISTS "specialties_unit_id_name_unique" ON "specialties" USING btree ("unit_id", "name");
+-- Migration already applied - index specialties_unit_id_name_unique already exists
